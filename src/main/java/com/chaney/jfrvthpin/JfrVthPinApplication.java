a27688fc45115da9a6e47abdd48c04bda88329cb
chaney <chaneychan@163.com>
2024-05-26 20:13:45 +0800

    init

---
+++ src/main/java/com/chaney/jfrvthpin/JfrVthPinApplication.java
@@ -0,0 +1,13 @@
+package com.chaney.jfrvthpin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfrVthPinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JfrVthPinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chaney/jfrvthpin/JfrVthPinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaney.jfrvthpin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfrVthPinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
